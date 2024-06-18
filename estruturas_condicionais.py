65dbdd86eca125ff0d0e6b5fd9a03bac43e7985b
Sullivan <ssc@outlook.com.br>
2024-06-18 15:01:02 -0300

    estruturas condicionais

--- estruturas_condicionais.py
+++ estruturas_condicionais.py
@@ -6,3 +6,29 @@ if idade >= 18:
     print('Você é maior de idade.')
 else:
     print('Você é menor de idade.')
+
+"""
+Imagine que você queira imprimir "Aprovado", caso o estudante tenha uma média superior
+ou igual a 7, e "Reprovado", caso a média seja inferior a 7.
+"""
+
+"""
+media = float(input('Informe a média do estudante: '))
+if media >= 7:
+    print('Você foi aprovado!')
+elif media >= 5:
+    print('Recuperação')
+else:
+    print('Você foi reprovado. ')
+
+"""
+
+media = 10
+presenca = 100
+
+if media >= 7 and presenca >= 70:
+    print('Aprovado')
+    print('Parabéns')
+else:
+    print('Reprovado')
+    print('Tente novamente')
