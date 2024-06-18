4306e0f29720dafd0a52702918067aa0005836a0
Sullivan <ssc@outlook.com.br>
2024-06-18 13:09:16 -0300

    first commit

---
+++ variaiveis.py
@@ -0,0 +1,17 @@
+# > Variáveis
+
+# 1. Variáveis
+idade = 26 # Criando uma variável
+altura = 1.77
+nome = 'Sullivan'
+estudando = True
+
+print (type(idade))
+print (type(altura))
+print (type(nome))
+print (type(estudando))
+
+# Obtendo dados do usuário e salvando variáveis
+
+linguagem = input('Qual é a linguagem que você está estudando? ')
+print(' A linguagem que você está estudando é', linguagem)
